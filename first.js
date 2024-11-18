32ea3ba7aa72ed3817cb25daf1392adcd0e4a6cb
enkhlen11 <enkhlen_11@yahoo.com>
2024-11-18 21:19:18 +0800

    home javascript

---
+++ first.js
@@ -0,0 +1,15 @@
+let drom = false;
+let score = 20;
+if (score > 18) {
+  if (drom == true) {
+    console.log("passed");
+  } else {
+    console.log("dromdoo tentseh heregtei");
+  }
+} else {
+  if (score < 18) {
+    console.log("dvremee unshih heregtei");
+  } else {
+    console.log("2uulangiin beldeh heregtei");
+  }
+}
